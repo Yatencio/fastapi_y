7401a28adcfbf153d6ce04e89dc90ac42e96a74a
Yatencio <yosira_ag@hotmail.com>
2020-06-13 10:32:43 -0500

    actualizacion, creacion de funcion

--- app/main.py
+++ app/main.py
@@ -10,3 +10,30 @@ def read_hola():
 @app.get("/mundo")
 def read_mundo():
     return {"hola": "hola migue"}
+
+@app.get("/realizar_flexiones/")
+def realizar_flexiones ():
+    # iniciar_flexiones = False
+
+    #entradas
+    # a.identificar accion para iniciar las flexiones
+    iniciar_flexiones = True #boolean
+
+    # b. cantidad de flexiones
+    flexiones_a_realizar = 20
+    
+    if iniciar_flexiones is True:
+        #procesamiento
+        #contador iniciando en cero
+        contador_de_flexiones = 0
+
+        # B. aumentar en 1 el acumulador por cada flexion realizada
+        while contador_de_flexiones < 20:
+            contador_de_flexiones += 1
+            print(f"flexion numero: {contador_de_flexiones}")
+
+    elif iniciar_flexiones is False: #tambien se puede usar el else
+        return {"mensaje": f"descansar"} 
+        
+    #salida
+    return {"mensaje":f"{flexiones_a_realizar}Flexiones realizadas"}
